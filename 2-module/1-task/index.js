f9274a6b8d0337a2faab6326f97fb6f33887a597
Igor Loboda <igore4ek061@gmail.com>
2021-06-13 17:36:38 +0300

    resolved tasks of module 3

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  for ( let key in salaries ) {
+    if ( !isNaN(salaries[key]) && salaries[key] !== Infinity && salaries[key] !== -Infinity  && typeof salaries[key] !== "boolean" ) {
+      result += salaries[key];
+    }
+  }
+  return result;
 }
