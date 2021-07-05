47c576c2f0e392532a2a01c3895cf271ddfc530c
Igor Loboda <igore4ek061@gmail.com>
2021-07-05 14:58:09 +0300

    module 4 tasks and solutions

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement("ul");
+  for ( let i in friends) {
+    let li = document.createElement("li");
+    ul.appendChild(li);
+    li.innerHTML = `${friends[i].firstName} ${friends[i].lastName}`;
+  }
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
-  // ваш код...
+  for ( let i = 0; i < table.tBodies[0].rows.length; i++ ) {
+    for ( let j = 0; j < table.tBodies[0].rows[i].cells.length; j++ ) {
+      let currentElement = table.tBodies[0].rows[i].cells[j];
+
+      if( currentElement.dataset.available == 'true') {
+        table.tBodies[0].rows[i].classList.add('available');
+      } else if (currentElement.dataset.available == 'false') {
+        table.tBodies[0].rows[i].classList.add('unavailable');
+      } else if (currentElement.innerHTML == 'm') {
+        table.tBodies[0].rows[i].classList.add('male');
+      } else if (currentElement.innerHTML == 'f') {
+        table.tBodies[0].rows[i].classList.add('female');
+      } else if (currentElement.innerHTML < 18 ) {
+        table.tBodies[0].rows[i].style.cssText = 'text-decoration: line-through';
+      } else if ( currentElement.cellIndex == 3 && currentElement.dataset.available == undefined) {
+        console.log(currentElement);
+        table.tBodies[0].rows[i].hidden='true';        
+      }
+
+    }
+  }
+
 }
